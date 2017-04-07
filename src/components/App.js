1846e8fdd434c5195f7297fba33043684b88ef30
gregmarquet <marquet.greg@gmail.com>
2017-04-07 10:11:41 -0400

    add components and stylesheets folder, change relative paths

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from '../logo.svg';
+import '../stylesheets/App.css';
+import Button from './Button.js'
+
 
 class App extends Component {
   render() {
@@ -8,9 +10,12 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>React Playground</h2>
         </div>
-        
+
+        <div className="counter">
+          <Button />
+        </div>
       </div>
     );
   }
