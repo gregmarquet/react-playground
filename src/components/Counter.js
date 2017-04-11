ad0747f8e19374c605f9bf7b5c9fcd02d2b0c9af
gregmarquet <marquet.greg@gmail.com>
2017-04-10 22:19:46 -0400

    create Counter component and add it to CounterApp

---
+++ src/components/Counter.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const Counter = (props) => {
+  return (
+    <div className="Counter-div">
+      <p>The total count is: </p>
+    </div>
+  )
+}
+
+export default Counter
--- src/components/CounterApp.js
+++ src/components/CounterApp.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import '../stylesheets/CounterApp.css';
 import Button from './Button.js';
+import Counter from './Counter.js';
 
 class CounterApp extends Component {
   render(){
     return(
       <div className='counter-app'>
         <Button />
+        <Counter />
       </div>
     )
   }
