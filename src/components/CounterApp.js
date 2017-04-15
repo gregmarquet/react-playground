e33a03258c5aad1ec6d15ab13592160caf9dcecb
gregmarquet <marquet.greg@gmail.com>
2017-04-15 06:37:00 -0400

    create 3 more button with different increments, add margin-right to button

--- src/components/CounterApp.js
+++ src/components/CounterApp.js
@@ -18,6 +18,9 @@ class CounterApp extends Component {
       <div className='counter-app'>
         <h4>Click a button to add to the total count.</h4>
         <Button handleClick={this.handleClick.bind(this)} increment={1} />
+        <Button handleClick={this.handleClick.bind(this)} increment={5} />
+        <Button handleClick={this.handleClick.bind(this)} increment={10} />
+        <Button handleClick={this.handleClick.bind(this)} increment={100} />
         <Counter totalCount={this.state.counter} />
       </div>
     )
